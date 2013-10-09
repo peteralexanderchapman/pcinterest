4abdedf38eefa659c8d892d33270494e972f23de
peteralexanderchapman <peter.chapman@ediply.com>
2013-10-09 11:15:05 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pcinterest::Application.config.secret_key_base = '61a6c696cdbc6f9d58a14ac25ffed2c1e869f618ba23ec2e3fa63fbd990b93768ec2ff03009438139bb02dc604b62154b6bbea06a04a2336e0ac871ad40afc18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pcinterest::Application.config.session_store :cookie_store, key: '_pcinterest_session'
